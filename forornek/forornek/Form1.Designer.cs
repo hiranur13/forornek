43bf40ee15ba7979de570aad7a5f2a06e9e8a639
hiranur <145452148+hiranur13@users.noreply.github.com>
2023-10-23 13:04:22 +0300

    Add files via upload

---
+++ forornek/forornek/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace forornek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnYazdir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBaslangic = new System.Windows.Forms.TextBox();
+            this.txtBitis = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnYazdir
+            // 
+            this.btnYazdir.Location = new System.Drawing.Point(121, 79);
+            this.btnYazdir.Name = "btnYazdir";
+            this.btnYazdir.Size = new System.Drawing.Size(141, 42);
+            this.btnYazdir.TabIndex = 0;
+            this.btnYazdir.Text = "Yazdır";
+            this.btnYazdir.UseVisualStyleBackColor = true;
+            this.btnYazdir.Click += new System.EventHandler(this.btnYazdir_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Başlangıç:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Bitiş:";
+            // 
+            // txtBaslangic
+            // 
+            this.txtBaslangic.Location = new System.Drawing.Point(121, 12);
+            this.txtBaslangic.Name = "txtBaslangic";
+            this.txtBaslangic.Size = new System.Drawing.Size(141, 29);
+            this.txtBaslangic.TabIndex = 3;
+            // 
+            // txtBitis
+            // 
+            this.txtBitis.Location = new System.Drawing.Point(121, 44);
+            this.txtBitis.Name = "txtBitis";
+            this.txtBitis.Size = new System.Drawing.Size(141, 29);
+            this.txtBitis.TabIndex = 4;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 24;
+            this.listBox1.Location = new System.Drawing.Point(278, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(147, 172);
+            this.listBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 206);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txtBitis);
+            this.Controls.Add(this.txtBaslangic);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnYazdir);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnYazdir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBaslangic;
+        private System.Windows.Forms.TextBox txtBitis;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ forornek/forornek/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace forornek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnYazdir_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            int sayi1=Convert.ToInt32(txtBaslangic.Text);
+            int sayi2=Convert.ToInt32(txtBitis.Text);
+            for (int i = sayi1; i <=sayi2; i++)
+            {
+                listBox1.Items.Add(i);
+            }
+        }
+    }
+}
